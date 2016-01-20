bcf6400c232b963dfc22e55a386538c74fccb859
Robert Carswell <rcarswel@westga.edu>
2016-01-20 14:56:50 -0500

    Created OnClick methods for plus and minus.

--- app/src/main/java/edu/westga/cs6242/countbuttonclicks/MainActivity.java
+++ app/src/main/java/edu/westga/cs6242/countbuttonclicks/MainActivity.java
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +48,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonOnClickPlus(View v) {
+        // do something when the button is clicked
+    }
+
+    public void buttonOnClickMinus(View v) {
+        // do something when the button is clicked
     }
 }
